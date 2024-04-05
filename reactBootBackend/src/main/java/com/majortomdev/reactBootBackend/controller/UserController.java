0826b803e1223e0bd48ca0967c39eb865a01341e
Joseph Kinahan <kinahanj25@gmail.com>
2024-04-05 22:50:19 +0100

    added edit and delete EPs to server, built out FE edit page, using useEffect to populate edit page with current stats per user

--- reactBootBackend/src/main/java/com/majortomdev/reactBootBackend/controller/UserController.java
+++ reactBootBackend/src/main/java/com/majortomdev/reactBootBackend/controller/UserController.java
@@ -31,5 +31,25 @@ public class UserController {
                 .orElseThrow(()->new UserNotFoundException(id));
     }
 
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id){
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setEmail(newUser.getEmail());
+                    user.setName(newUser.getName());
+                    return userRepository.save(user);
+                }).orElseThrow(()->new UserNotFoundException(id));
+    }
 
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id){
+        if(!userRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+        else {
+            userRepository.deleteById(id);
+            return "Deleted user id: "+id;
+        }
+    }
 }
